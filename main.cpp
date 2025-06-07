2c148686dbfa0ecbfb0be726a93d22bff0b08268
XnoobSpeakable <xnoobspeakable@gmail.com>
2025-06-07 12:32:17 +0300

    fly

--- main.cpp
+++ main.cpp
@@ -21,6 +21,16 @@ $hook(void, Player, update, World* world, double dt, EntityPlayer* entityPlayer)
 	original(self, world, dt, entityPlayer);
 }
 
+$hook(void, Player, updatePos, World* world, double dt)
+{
+	if (!(BlockInfo::TYPE)world->getBlock(Player::pos + Player::vel*dt)==BlockInfo::AIR  && ((BlockInfo::TYPE)world->getBlock(Player::pos + Player::vel*dt + glm::vec(0,1,0,0)))==BlockInfo::AIR) {
+		Player::pos.y += 1;
+
+	}
+	original(self, world, dt);
+}
+
+
 $hook(bool, Player, keyInput, GLFWwindow* window, World* world, int key, int scancode, int action, int mods)
 {
 	// Your code that runs when Key Input happens (check GLFW Keyboard Input tutorials)|(it only calls when you play in world, because it is a Player function)
