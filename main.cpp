e4c58d644c6e197a160b13f957630ff2908433a3
XnoobSpeakable <xnoobspeakable@gmail.com>
2025-06-08 10:37:53 +0300

    tr1ngle made this lol

--- main.cpp
+++ main.cpp
@@ -7,23 +7,60 @@ using namespace fdm;
 // Initialize the DLLMain
 initDLL
 
-//ZiHEd mostly made this lol, XnoobSpeakable just a little bit of it
+glm::vec4 safeNormalize(const glm::vec4& v)
+{
+	float sqrLength = glm::dot(v, v);
+	if (sqrLength <= 0.0001f * 0.0001f)
+		sqrLength = 1.0f;
+	float length = glm::sqrt(sqrLength);
+	return v * (1.0f / length);
+}
+
+// ZiHEd and Tr1Ngle mostly made this lol, XnoobSpeakable just a little bit of it
 $hook(void, Player, updatePos, World* world, double dt)
 {
-    if (self != &fdm::StateGame::instanceObj.player) return;
+	// make sure it is the local player
+	if (self != &fdm::StateGame::instanceObj.player) return original(self, world, dt);
 
-    glm::vec4 nextPos = self->pos + self->vel*(float)dt + glm::vec4{0,0.001f,0,0}; // optional - add 0.001f just in case, floats can be funny
-    glm::vec4 nextUpPos = nextPos + glm::vec4(0,1,0,0);
-	glm::vec4 nextDiUpPos = nextPos + glm::vec4(0,2,0,0);
+	// weird length formula from updatePos
+	float length = glm::length(self->vel)
+		+ (glm::length(self->deltaVel) * 0.5f) * (dt * dt);
 
-    bool isNextBlockSolid = BlockInfo::Blocks.at(world->getBlock(nextPos)).solid;
-    bool isNextUpBlockSolid = BlockInfo::Blocks.at(world->getBlock(nextUpPos)).solid; 
-	bool isNextDiUpBlockSolid = BlockInfo::Blocks.at(world->getBlock(nextDiUpPos)).solid; 
-    // also check if the upper block is solid, not air, so that you can step on blocks even if there are bushes on it
+	// ray dir
+	glm::vec4 dir = safeNormalize(self->vel + self->deltaVel);
+	// ray origin
+	glm::vec4 origin = self->pos + glm::vec4{ 0,0.05f,0,0 }; // add a tiny offset up
 
-    if ( isNextBlockSolid && !isNextUpBlockSolid && !isNextDiUpBlockSolid ) self->pos.y += 1;
-    
-    original(self, world, dt);
+	// don't step if moving down or if not moving
+	if (glm::dot(dir, { 0, 1, 0, 0 }) <= -0.01f || length <= 0.0001f)
+		return original(self, world, dt);
+
+	glm::ivec4 curBlock = glm::floor(origin);
+	glm::vec4 endpoint = origin + dir * length * (float)dt;
+	glm::ivec4 intersection = glm::floor(endpoint);
+	// don't do anything if the intersection is on the same block as the origin
+	if (curBlock == intersection) return original(self, world, dt);
+
+	// raycast to check for a wall
+	if (world->castRay(origin, curBlock, intersection, endpoint))
+	{
+		uint8_t intersectionBlock = world->getBlock(intersection);
+		if (intersection.y == glm::floor(origin.y) &&
+			BlockInfo::Blocks[intersectionBlock].solid &&
+			!BlockInfo::Blocks[world->getBlock(intersection + glm::ivec4{0,1,0,0})].solid &&
+			!BlockInfo::Blocks[world->getBlock(intersection + glm::ivec4{0,2,0,0})].solid)
+		{
+			// move the player onto the block
+			self->pos += dir * length * (float)dt;
+			self->pos.y = intersection.y + 1;
+			// update currentBlock
+			self->currentBlock = glm::round(self->pos);
+			self->groundBlock = intersectionBlock;
+			self->touchingGround = true;
+		}
+	}
+ 
+	return original(self, world, dt);
 }
 
 
